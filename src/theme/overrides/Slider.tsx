7030256dcd09a61ebc4c90ab48400fd32993b158
Anwarkamal143 <anwarkamal1434@gmail.com>
2023-02-02 02:43:13 +0500

    updated theme typography

--- src/theme/overrides/Slider.tsx
+++ src/theme/overrides/Slider.tsx
@@ -2,8 +2,6 @@
 import { Theme } from "@mui/material/styles";
 
 export default function Slider(theme: Theme) {
-  const isLight = theme.palette.mode === "light";
-
   return {
     MuiSlider: {
       defaultProps: {},
--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -6,9 +6,6 @@ import { createTheme, Palette } from "@mui/material/styles";
 export const defualtTheme = createTheme({
   spacing: (factor: number) => `${0.25 * factor}rem`,
 });
-function createGradient(color1: string, color2: string) {
-  return `linear-gradient(to bottom, ${color1}, ${color2})`;
-}
 
 // SETUP COLORS
 const palette: Record<string, Palette> = {
