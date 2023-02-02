bfc920eee981980610a477fa32d3546df28839ba
Anwarkamal143 <anwarkamal1434@gmail.com>
2023-02-03 02:10:08 +0500

    updated button story

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -1,3 +1,4 @@
+import BtnUnstyled from "@mui/base/ButtonUnstyled";
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 import Button from "components/Button";
 export default {
@@ -9,3 +10,8 @@ export const Default: ComponentStory<typeof Button> = (props) => {
   const { children = "Button", ...rest } = props;
   return <Button {...rest}>{children}</Button>;
 };
+
+export const ButtonUnstyled: ComponentStory<typeof BtnUnstyled> = (props) => (
+  <BtnUnstyled {...props}>Hello</BtnUnstyled>
+);
+Default.argTypes = {};
