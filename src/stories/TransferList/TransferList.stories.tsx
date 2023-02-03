b32b7f071765b9b4483d28a78c0ea3d4ac9c1ec2
Umair Manzoor <umairmanzoor1145@gmail.com>
2023-02-04 03:21:35 +0500

    transferlist tooltip tooglebutton timeline

--- src/stories/TransferList/TransferList.stories.tsx
+++ src/stories/TransferList/TransferList.stories.tsx
@@ -1,16 +1,16 @@
-import Button from "@mui/material/Button";
-import Checkbox from "@mui/material/Checkbox";
-import Grid from "@mui/material/Grid";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Paper from "@mui/material/Paper";
-import { ComponentMeta } from "@storybook/react";
-import * as React from "react";
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
+import { ComponentMeta } from '@storybook/react';
+import * as React from 'react';
 
 export default {
-  title: "Components/TransferList",
+  title: 'Components/TransferList',
   component: ListItem,
 } as ComponentMeta<typeof ListItem>;
 
@@ -65,29 +65,31 @@ export const MuiTransferList = () => {
     setRight([]);
   };
 
-  const customList = (items: readonly number[]) => (
-    <Paper sx={{ width: 200, height: 230, overflow: "auto" }}>
-      <List dense component="div" role="list">
+  const customList = (items: readonly number[], checkBox: boolean) => (
+    <Paper sx={{ width: 200, height: 230, overflow: 'auto' }}>
+      <List dense component='div' role='list'>
         {items.map((value: number) => {
           const labelId = `transfer-list-item-${value}-label`;
 
           return (
             <ListItem
               key={value}
-              role="listitem"
+              role='listitem'
               button
               onClick={handleToggle(value)}
             >
-              <ListItemIcon>
-                <Checkbox
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{
-                    "aria-labelledby": labelId,
-                  }}
-                />
-              </ListItemIcon>
+              {checkBox && (
+                <ListItemIcon>
+                  <Checkbox
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{
+                      'aria-labelledby': labelId,
+                    }}
+                  />
+                </ListItemIcon>
+              )}
               <ListItemText id={labelId} primary={`List item ${value + 1}`} />
             </ListItem>
           );
@@ -97,53 +99,53 @@ export const MuiTransferList = () => {
   );
 
   return (
-    <Grid container spacing={2} justifyContent="center" alignItems="center">
-      <Grid item>{customList(left)}</Grid>
+    <Grid container spacing={2} justifyContent='center' alignItems='center'>
+      <Grid item>{customList(left, false)}</Grid>
       <Grid item>
-        <Grid container direction="column" alignItems="center">
+        <Grid container direction='column' alignItems='center'>
           <Button
             sx={{ my: 0.5 }}
-            variant="outlined"
-            size="small"
+            variant='outlined'
+            size='small'
             onClick={handleAllRight}
             disabled={left.length === 0}
-            aria-label="move all right"
+            aria-label='move all right'
           >
             ≫
           </Button>
           <Button
             sx={{ my: 0.5 }}
-            variant="outlined"
-            size="small"
+            variant='outlined'
+            size='small'
             onClick={handleCheckedRight}
             disabled={leftChecked.length === 0}
-            aria-label="move selected right"
+            aria-label='move selected right'
           >
             &gt;
           </Button>
           <Button
             sx={{ my: 0.5 }}
-            variant="outlined"
-            size="small"
+            variant='outlined'
+            size='small'
             onClick={handleCheckedLeft}
             disabled={rightChecked.length === 0}
-            aria-label="move selected left"
+            aria-label='move selected left'
           >
             &lt;
           </Button>
           <Button
             sx={{ my: 0.5 }}
-            variant="outlined"
-            size="small"
+            variant='outlined'
+            size='small'
             onClick={handleAllLeft}
             disabled={right.length === 0}
-            aria-label="move all left"
+            aria-label='move all left'
           >
             ≪
           </Button>
         </Grid>
       </Grid>
-      <Grid item>{customList(right)}</Grid>
+      <Grid item>{customList(right, false)}</Grid>
     </Grid>
   );
 };
